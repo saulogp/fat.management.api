c54647a8fcff65428b83b3c7487c52ed50beb4fe
saulogp <sauloproetti@gmail.com>
2023-07-08 13:54:39 -0300

    feat: autenticação dos endpoints com jwt e melhorias do swagger

--- Presentation/Controller/v1/AuthenticationRouter.cs
+++ Presentation/Controller/v1/AuthenticationRouter.cs
@@ -6,6 +6,7 @@ using Infrastructure.Data.Interface;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using System.Text;
 
 namespace Presentation.Controller
 {
@@ -16,20 +17,19 @@ namespace Presentation.Controller
             var authGroup = app.MapGroup("api/v1");
             authGroup.MapPost("auth/login", Login);
             authGroup.MapPost("auth/register", Register);
-            authGroup.MapPost("auth/changePassword", ChangePassword).RequireAuthorization();
+            authGroup.MapPost("auth/changePassword", ChangePassword);
         }
 
         private static async Task<IResult> Login(UserAuthenticateModel request, [FromServices] IUserProfileRepository repository)
         {
             var user = await repository.GetUserAsync(request.Email);
 
-            if (user == null) return Results.BadRequest("Not Allow");
+            if (user is null) return Results.Unauthorized();
 
             if (!VerifyPasswordHash(request.Password, user.PasswordHash, user.PasswordSalt))
-                return Results.BadRequest("Not Allow");
+                return Results.Unauthorized();
 
-            var token = CreateToken(user);
-            return Results.Ok(token);
+            return Results.Ok(user.CreateToken());
         }
 
         private static async Task<IResult> ChangePassword(UserUpdatePasswordModel request, [FromServices] IUserProfileRepository repository)
@@ -47,7 +47,7 @@ namespace Presentation.Controller
 
             await repository.UpdateAsync(user.Id, user);
 
-            return Results.Ok("Allow");
+            return Results.Ok();
         }
 
         private static async Task<IResult> Register(UserRegisterModel request, [FromServices] IUserProfileRepository repository)
@@ -57,10 +57,10 @@ namespace Presentation.Controller
 
             var username = await repository.GetUsernameAsync(request.Username);
             if (username is not null) return Results.BadRequest("This username is already being used");
-         
+
             CreatePasswordHash(request.Password, out byte[] passwordHash, out byte[] passwordSalt);
             await repository.CreateAsync(new UserProfileEntity { Id = Guid.NewGuid(), UserName = request.Username, Email = request.Email, PasswordHash = passwordHash, PasswordSalt = passwordSalt, CreatedDate = DateTime.UtcNow, LastUpdatedDate = DateTime.UtcNow });
-         
+
             return Results.Ok();
         }
 
@@ -82,24 +82,36 @@ namespace Presentation.Controller
             }
         }
 
-        private static string CreateToken(UserProfileEntity user){
-            List<Claim> claims = new List<Claim>{
-                new Claim(ClaimTypes.Name, user.UserName)
+        private static string CreateToken(this UserProfileEntity user)
+        {
+            var issuer = "test.com";
+            var audience = "test.com";
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("&2f5t@dmR_8*79WtoSmCY1BzC"));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+            
+            var jwtTokenHandler = new JwtSecurityTokenHandler();
+
+            var key = Encoding.ASCII.GetBytes("&2f5t@dmR_8*79WtoSmCY1BzC");
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                    new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                    new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+                }),
+                Expires = DateTime.UtcNow.AddMinutes(2),
+                Audience = audience,
+                Issuer = issuer,
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha512Signature)
             };
 
-            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes("çkljasdçfkjaçsdfkj"));
-            
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var token = jwtTokenHandler.CreateToken(tokenDescriptor);
 
-            var token = new JwtSecurityToken(
-                claims: claims,
-                expires: DateTime.Now.AddMinutes(15),
-                signingCredentials: creds
-            );
+            var jwtToken = jwtTokenHandler.WriteToken(token);
 
-            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
-            
-            return jwt;
+            return jwtToken;
         }
     }
 }
